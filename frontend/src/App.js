8132161c71e91801b69076aa787ba2549f0001ef
Rajan Kumar <rajansukul20@gmail.com>
2026-04-22 17:37:34 +0530

    connected frontend with live backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const API = "http://127.0.0.1:8000";
+  const API = "https://student-api-25x0.onrender.com";
 
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [login, setLogin] = useState({
